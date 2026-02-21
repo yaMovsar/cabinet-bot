7f92734d29882d4773e8d13e3f001435004475ae
yaMovsar <ismailova.ima@mail.ru>
2026-02-21 13:45:05 +0300

    fix config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8543600432:AAERq0yGGa0usBrRRUomyeJMpDrSYb7fxcU")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "741468078  "))
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 MANAGER_ID = int(os.getenv("MANAGER_ID", "0"))
