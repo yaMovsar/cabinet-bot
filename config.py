6f380ac6b442d085a775fae83b7b087e78d8544c
yaMovsar <ismailova.ima@mail.ru>
2026-02-21 13:51:35 +0300

    fix quotes in config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
-MANAGER_ID = int(os.getenv("MANAGER_ID", "0"))
+BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip().strip('"')
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0").strip().strip('"'))
+MANAGER_ID = int(os.getenv("MANAGER_ID", "0").strip().strip('"'))
